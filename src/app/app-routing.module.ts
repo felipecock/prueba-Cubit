a18e90bbb83d38f4c13e0405f9e87fb279ade615
Felipe Cock <felipecock@gmail.com>
2020-11-23 17:05:34 -0500

    versión alfa 0.4.1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BienvenidoComponent } from './bienvenido/bienvenido.component';
+import { PreguntasComponent } from './preguntas/preguntas.component';
+import { DetalleUsuarioComponent } from './usuarios/componentes/detalle-usuario/detalle-usuario.component';
+import { ListaUsuariosComponent } from './usuarios/componentes/lista-usuarios/lista-usuarios.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'lista-usuarios', component: ListaUsuariosComponent },
+  { path:'detalle-usuario', component: DetalleUsuarioComponent },
+  { path:'preguntas', component: PreguntasComponent },
+  { path:'', component: BienvenidoComponent , pathMatch: 'full' }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,14 @@ import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { UsuariosModule } from './usuarios/usuarios.module';
 import { PreguntasComponent } from './preguntas/preguntas.component';
-import { ListaUsuariosComponent } from './usuarios/componentes/lista-usuarios/lista-usuarios.component';
 import { BienvenidoComponent } from './bienvenido/bienvenido.component';
 import { EstadoActualService } from './home/estado-actual.service';
-
-const rutasApp:Routes = [
-  { path:'lista-usuarios', component: ListaUsuariosComponent },
-  { path:'preguntas', component: PreguntasComponent },
-  { path:'', component: BienvenidoComponent , pathMatch: 'full' }
-]
+import { DetalleUsuarioComponent } from './usuarios/componentes/detalle-usuario/detalle-usuario.component';
 
 @NgModule({
   declarations: [
@@ -28,9 +21,9 @@ const rutasApp:Routes = [
     NavbarComponent,
     PreguntasComponent,
     BienvenidoComponent,
+    DetalleUsuarioComponent
   ],
   imports: [
-    RouterModule.forRoot(rutasApp),
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
